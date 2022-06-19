a336b85a97552ec1a929aefe8bc193b07a1eba7a
naghipourfar <naghipourfar@berkeley.edu>
2022-06-19 10:27:40 -0700

    simple version of fetch added

--- b10/data/__init__.py
+++ b10/data/__init__.py
@@ -1,7 +1,13 @@
 from typing import Optional
+from ._connection import DatabaseConnection
+
+db_connection = None
 
 
 def fetch(limit: Optional[int] = 1000, **query_params):
-    pass
+    global db_connection
+    if db_connection is None:
+        db_connection = DatabaseConnection()
 
-
+    query_results = db_connection.query(**query_params, limit=limit)
+    return query_results
