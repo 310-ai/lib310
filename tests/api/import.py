cccf71b5cd64810d43ad9c53bd1754cb4b831d90
saman <samanf74@gmail.com>
2022-06-27 17:22:16 +0200

    now the test of import passed

--- tests/api/import.py
+++ tests/api/import.py
@@ -1,5 +1,8 @@
 def test_b10_import():
     try:
-        from b10 import dataAPI as d, MLAPI as m
+        from b10 import db
+        from b10 import MLAPI
+        from b10 import pl
+        from b10 import tl
     except ImportError as e:
         raise e
