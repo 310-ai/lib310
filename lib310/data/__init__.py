5cb2ffee972a1dadb75a55810e7926e923fad915
naghipourfar <mohsen.naghipourfar@gmail.com>
2022-07-01 03:19:16 +0000

    ProteinDataTable added

--- lib310/data/__init__.py
+++ lib310/data/__init__.py
@@ -1,23 +1,8 @@
-from typing import Optional
-from ._connection import DatabaseConnection, set_gcloud_key_path
+import pandas as pd
 
-db_connection: DatabaseConnection = None
-
-
-def fetch(*args, **kwargs):
-    global db_connection
-    if db_connection is None:
-        db_connection = DatabaseConnection(**kwargs)
-
-    query_results = db_connection.query(*args, **kwargs)
-    return query_results
+from ._base import ProteinDataTable
 
 
-def get_table_info(**kwargs):
-    global db_connection
-    if db_connection is None:
-        db_connection = DatabaseConnection(**kwargs)
-    elif db_connection.table_name != kwargs.get('table_name', db_connection.table_name):
-        db_connection = DatabaseConnection(**kwargs)
-
-    return db_connection.get_table_info()
+def read_csv(*args, **kwargs):
+    df = pd.read_csv(*args, **kwargs)
+    return ProteinDataTable.from_pandas(df)
--- lib310/data/exceptions.py
+++
@@ -1,4 +0,0 @@
-class VolumeLimitException(Exception):
-
-    def __init__(self, message="The query uses volume more than our limit"):
-        super().__init__(message)
