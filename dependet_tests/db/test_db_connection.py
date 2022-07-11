f96497af20ad08b2524cf051606a1a040c89575c
saman <samanf74@gmail.com>
2022-07-11 15:36:02 +0430

    adding get_datasets and get_tables

--- dependet_tests/db/test_db_connection.py
+++ dependet_tests/db/test_db_connection.py
@@ -12,5 +12,14 @@ class TestDB(unittest.TestCase):
         seqs = lib310.db.fetch(query="SELECT * FROM `pfsdb3.0_go.gaf` LIMIT 10")
         self.assertEqual(len(seqs), 10)
 
-    def test_get_datasets(self):
-        datasets = lib310.db.get_datasets()
+    def test_list_datasets(self):
+        datasets = lib310.db.list_datasets()
+        self.assertGreater(len(datasets), 0)
+        datasets_ids = [d.dataset_id for d in datasets]
+        self.assertIn('0_go', datasets_ids)
+
+    def test_list_tables(self):
+        tables = lib310.db.list_tables()
+        self.assertGreater(len(tables), 0)
+        tables = lib310.db.list_tables(dataset_ids='0_go')
+        self.assertGreater(len(tables), 0)
--- lib310/database/__init__.py
+++ lib310/database/__init__.py
@@ -23,15 +23,29 @@ def get_table_info(**kwargs):
     return db_connection.get_table_info()
 
 
-def get_datasets(**kwargs):
+def list_datasets(**kwargs):
     global db_connection
     if db_connection is None:
         db_connection = DatabaseConnection(**kwargs)
     return list(db_connection.client.list_datasets())
 
 
-def get_datasets(**kwargs):
+def list_tables(**kwargs):
     global db_connection
     if db_connection is None:
         db_connection = DatabaseConnection(**kwargs)
-    return list(db_connection.client.list_datasets())
+
+    dataset_ids = kwargs.get('dataset_ids')
+
+    if dataset_ids is None:
+        dataset_ids = [d.dataset_id for d in list(db_connection.client.list_datasets())]
+
+    if not isinstance(dataset_ids, list):
+        dataset_ids = [dataset_ids]
+
+    if len(dataset_ids) == 1:
+        return list(db_connection.client.list_tables(dataset_ids[0]))
+    result = {}
+    for dataset_id in dataset_ids:
+        result[dataset_id] = list(db_connection.client.list_tables(dataset_id))
+    return result
