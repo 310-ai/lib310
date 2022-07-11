8d050a371943fe2f40dddda89335805fc826e75b
saman <samanf74@gmail.com>
2022-07-11 13:23:20 +0430

    ignore the usage log in test

--- tests/api/test_db_connection.py
+++ tests/api/test_db_connection.py
@@ -1,2 +1,7 @@
 def test_bigquery_select():
+    import lib310
+    import dotenv
+    dotenv.load_dotenv('.env')
+    seqs = lib310.db.fetch(query="SELECT * FROM `pfsdb3.0_go.gaf` LIMIT 10")
+    print(seqs)
     pass
