363d3ef3ad1a39bbbdbdbfbee03ce6378b53478d
inaderi268 <inaderi@310.ai>
2022-09-01 13:51:52 -0600

    add pdb_api_wrapper

--- lib310/tools/_pdb.py
+++ lib310/tools/_pdb.py
@@ -14,11 +14,7 @@ def download(pdb_id, file_type, out_dir):
 
 def download_list_pdb(pdb_list, type_list, out_dir):
     import os
-    try:
-        os.makedirs(out_dir)
-    except FileExistsError:
-        print('directory already exists')
-
+    os.makedirs(out_dir, exist_ok=True)
     for pdb in pdb_list:
         for tp in type_list:
             download(pdb, tp, out_dir)
