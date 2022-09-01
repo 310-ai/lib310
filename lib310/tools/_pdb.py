fea38ce30ad0db436b07c38d57979c3974ecc7a2
inaderi268 <inaderi@310.ai>
2022-09-01 13:44:22 -0600

    add pdb_api_wrapper

--- lib310/tools/_pdb.py
+++ lib310/tools/_pdb.py
@@ -1,8 +1,5 @@
 import requests
-
 BASE_URL = "https://files.rcsb.org/download"
-
-
 def download(pdb_id, file_type, out_dir):
     if pdb_id.find('_'):
         pdb_id = pdb_id[:-2]
@@ -10,12 +7,8 @@ def download(pdb_id, file_type, out_dir):
     res = requests.get(url)
     outfile = "{out_dir}/{pdb_id}.{type}.gz".format(pdb_id=pdb_id, type=file_type, out_dir=out_dir)
     open(outfile, "wb").write(res.content)
-
-
 def download_list_pdb(pdb_list, type_list, out_dir):
     for pdb in pdb_list:
         for tp in type_list:
             download(pdb, tp, out_dir)
-
-
 # download_list_pdb(["7niu_A", "6s7p_A"], ["pdb", "pdb1", "xml"],"./data")
