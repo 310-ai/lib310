acf22b5f849ae0c22f3b24a91b55bbf369933cf0
inaderi268 <inaderi@310.ai>
2022-09-01 13:15:03 -0600

    add pdb_api_wrapper

--- lib310/tools/_pdb.py
+++ lib310/tools/_pdb.py
@@ -3,21 +3,24 @@ import requests
 BASE_URL = "https://files.rcsb.org/download"
 
 
-def download(pdb_id, type,outdir):
+def download(pdb_id, file_type, out_dir):
     if pdb_id.find('_'):
         pdb_id = pdb_id[:-2]
-    url = "{BASE_URL}/{pdb_id}.{type}.gz".format(pdb_id=pdb_id, BASE_URL=BASE_URL, type=type)
+    url = "{BASE_URL}/{pdb_id}.{type}.gz".format(pdb_id=pdb_id, BASE_URL=BASE_URL, type=file_type)
     res = requests.get(url)
-    outfile = "{outdir}/{pdb_id}.{type}.gz".format(pdb_id=pdb_id, type=type, outdir=outdir)
+    outfile = "{out_dir}/{pdb_id}.{type}.gz".format(pdb_id=pdb_id, type=file_type, outdir=out_dir)
     open(outfile, "wb").write(res.content)
 
 
-def download_list_pdb(pdb_list, type_list,outdir):
+def download_list_pdb(pdb_list, type_list, out_dir):
     import os
-    os.mkdir(outdir)
+    dir_exist = os.path.exists(out_dir)
+    if not dir_exist:
+        # Create a new directory because it does not exist
+        os.mkdir(out_dir)
     for pdb in pdb_list:
         for tp in type_list:
-            download(pdb, tp,outdir)
+            download(pdb, tp, out_dir)
 
 
 # download_list_pdb(["7niu_A", "6s7p_A"], ["pdb", "pdb1", "xml"],"./data")
