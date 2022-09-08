24800e6e1e1bace2d990b0de87e3025b6cd9189b
inaderi268 <inaderi@310.ai>
2022-09-07 22:34:03 -0600

    ffix pdb_api_wrapper

--- lib310/tools/__init__.py
+++ lib310/tools/__init__.py
@@ -1,4 +1,4 @@
 from ._clustering import cluster
-from ._pdb import download_list_pdb
+from ._pdb import download_list_pdb, get_pdb_parser
 
-__all__ = ['cluster','download_list_pdb']
+__all__ = ['cluster','download_list_pdb','get_pdb_parser']
