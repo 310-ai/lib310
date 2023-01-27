3588e4f4435f50d2e76d263036fb7a098e38330a
inaderi268 <inaderi@310.ai>
2023-01-26 20:33:08 -0700

    add_tm_score

--- lib310/tools/_tmalign.py
+++ lib310/tools/_tmalign.py
@@ -18,22 +18,16 @@ def tm_align( pdb_file_1,pdb_file_2):
 
 
 def tm_score( pdb_file_1,pdb_file_2):
-    import os.path
-    x = os.getcwd()
-    print("d")
-    s = x+'/TMScore.cpp'
-    if (os.path.exists(r'TMscore')) == False:
-        os.system('g++ -O3 -ffast-math -lm -o TMscore {s}'.format(s = s))
-    batcmd = 'ls'
+    import os
+    import subprocess
 
-    print(subprocess.getoutput(batcmd))
-    c = subprocess.getoutput(batcmd)
-    batcmd = './TMscore {file1} {file2}'.format(file1=pdb_file_1, file2=pdb_file_2)
-    # result = subprocess.check_output(batcmd, shell=True)
-    result2 = subprocess.getoutput(batcmd)
-    # print("result::: ", result)
-    # result2[result2.find('TM-score    '):result2.find('TM-score    ') + 32]
-    return c + result2
+    if (os.path.exists(r'TMscore')) == False:
+        print(subprocess.getoutput('g++ -O3 -ffast-math -lm -o TMscore {s}'.format(s='TMscore.cpp')))
+
+    batcmd = './TMscore {file1} {file2}'.format(file1="7ok9.pdb", file2="2gtl.pdb")
+
+    result = subprocess.getoutput(batcmd)
+    return result
 
 
 
