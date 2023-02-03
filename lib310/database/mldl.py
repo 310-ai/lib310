7534be3dd2e2e2793e5711a2716f979975ddd6ba
saman <samanf74@gmail.com>
2023-02-03 23:28:30 +0100

    reduce size of buffer for sample test

--- lib310/database/mldl.py
+++ lib310/database/mldl.py
@@ -61,7 +61,7 @@ class MLDL:
 
         if min_num_feature > 11:
             c = col.find({'len': {'$lt': max_length + 1}, 'token_size': {'$gt': min_num_feature - 1}},
-                         {'row_id': 1, '_id': 0}).sort("rand").limit(10 * num)
+                         {'row_id': 1, '_id': 0}).sort("rand").limit(5 * num)
             self.sample_cache = list(map(lambda x: x['row_id'], list(c)))
             self.filler_thread_cache = Thread(target=self.background_sample_cache,
                                               args=(max_length, num, min_num_feature, col))
@@ -95,7 +95,7 @@ class MLDL:
                 break
 
     def background_sample_cache(self, max_length, num, min_num_feature, col):
-        i = 10
+        i = 5
         while i > 0:
             c = col.find({'len': {'$lt': max_length + 1}, 'token_size': {'$gt': min_num_feature - 1}},
                          {'row_id': 1, '_id': 0}).sort("rand").skip(i * num).limit(num)
