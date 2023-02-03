1eda7fab4f98ece878f4a95c732ee2373fadaa37
saman <samanf74@gmail.com>
2023-02-03 11:48:56 +0100

    adding the background fill for the sample

--- lib310/database/mldl.py
+++ lib310/database/mldl.py
@@ -5,6 +5,7 @@ import pandas as pd
 from threading import Thread, Event
 from queue import Queue
 
+
 class MLDL:
     def __init__(self, mongo_url, cache_size=10):
         random.seed(time.time())
@@ -19,6 +20,8 @@ class MLDL:
         self.queue = Queue(self.max_queue_size)
         self.filler_thread = None
         self.kill_event = Event()
+        self.kill_event_cache = Event()
+        self.filler_thread_cache = None
 
         tmp = self.db['length_freq_lang5_train'].find().sort('len', ASCENDING)
         for item in tmp:
@@ -41,16 +44,27 @@ class MLDL:
                 return pd.DataFrame()
             max_length = max(arr)
 
-        hit = self.queue_key == f'{num}_{max_length}_{stage}'
+        hit = self.queue_key == f'{num}_{max_length}_{min_num_feature}_{stage}'
         if hit:
+            # HIT
             return pd.DataFrame(self.queue.get())
 
+        # MISS
         maxi = self.cache[stage.upper()][max_length]['maxi']
+
+        if self.filler_thread_cache is not None:
+            self.kill_event_cache.set()
+            self.filler_thread_cache.join()
+            self.kill_event_cache.clear()
+            self.filler_thread_cache = None
 
         if min_num_feature > 11:
             c = col.find({'len': {'$lt': max_length + 1}, 'token_ids': {'$size': min_num_feature}},
-                         {'row_id': 1, '_id': 0})
+                         {'row_id': 1, '_id': 0}).limit(10 * num)
             self.sample_cache = list(map(lambda x: x['row_id'], list(c)))
+            self.filler_thread_cache = Thread(target=self.background_sample_cache,
+                                              args=(max_length, num, min_num_feature, col))
+            self.filler_thread_cache.start()
         else:
             self.sample_cache = range(1, maxi)
 
@@ -61,7 +75,7 @@ class MLDL:
             self.kill_event.clear()
             self.queue = Queue(self.max_queue_size)
 
-        self.queue_key = f'{num}_{max_length}_{stage}'
+        self.queue_key = f'{num}_{max_length}_{min_num_feature}_{stage}'
 
         self.filler_thread = Thread(target=self.filler, args=(self.queue, num, self.sample_cache, col, self.kill_event))
         self.filler_thread.start()
@@ -80,8 +94,28 @@ class MLDL:
             if event.is_set():
                 break
 
+    def background_sample_cache(self, max_length, num, min_num_feature, col):
+        print('background_sample_cache')
+        i = 1
+        while i > 0:
+            print(i)
+            c = col.find({'len': {'$lt': max_length + 1}, 'token_ids': {'$size': min_num_feature}},
+                         {'row_id': 1, '_id': 0}).skip(10 * i * num).limit(10 * num)
+            tmp = list(c)
+            self.sample_cache.append(list(map(lambda x: x['row_id'], tmp)))
+            i += 1
+            if len(tmp) < 10 * num or self.kill_event_cache.is_set():
+                i = 0
+                break
+        print('background_sample_cache done')
+        # self.sample_cache =
+
     def terminate(self):
         self.kill_event.set()
         self.queue.get()
         self.filler_thread.join()
         self.kill_event.clear()
+        self.kill_event_cache.set()
+        self.filler_thread_cache.join()
+        self.kill_event_cache.clear()
+        self.filler_thread_cache = None
